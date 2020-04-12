552d8768b435a78673ec7402e839a6ba6f6e0a3a
wanderson jesus <andersonpeje46@gmail.com>
2020-04-12 08:47:41 -0300

    projeto criado

---
+++ src/wille/Wille.java
@@ -0,0 +1,19 @@
+
+package wille;
+
+
+public class Wille {
+
+   
+    public static void main(String[] args) {
+        
+        int ctt=100;
+        
+        while (ctt>0){
+            ctt--;
+            System.out.println("CONTADOR  NUMERO  ::   "   +   ctt);
+        }
+       
+    }
+    
+}
